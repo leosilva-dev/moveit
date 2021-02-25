fe8c2ea33afcb3f008e41e96ac9f8abe33f9ff17
leonardo silva <silvaleonardo.ti@gmail.com>
2021-02-25 14:42:49 -0300

    Finalização da aula 3

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
+import { ChallengesContexts } from '../contexts/ChallengesContexts';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+    const { currentExperience, experienceToNextLevel } = useContext(ChallengesContexts)
+
+    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel
+
     return (
         <header className={styles.experienceBar}>
             <span>0px</span>
             <div>
-                <div style={{ width: '50%' }}>
-                    <span className={styles.currentExperience} style={{ left: '50%' }}>
-                        300 xp
+                <div style={{ width: `${percentToNextLevel}%` }}>
+                    <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+                        {currentExperience} xp
                     </span>
                 </div>
             </div>
-            <span>600px</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
     )
 }
